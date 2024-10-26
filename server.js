9b5a1d659fd7545c39ac24c3ab5553c8542a6b1d
chandan vishwakarma <chandanvishw11002@gmail.com>
2024-10-26 21:50:18 +0530

    vercel config

--- server.js
+++ server.js
@@ -2,30 +2,21 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
-const path = require("path")
-if (process.env.NODE_ENV == "local") {
-    app.use(
-      cors({
-        origin: "http://localhost:3000",
-        credential: true,
-      })
-    );
-  } else {
-    app.use(
-      cors({
-        credential: true,
-      })
-    );
-  }
-  
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "./portfolio/dist")));
-    app.get("*",(req,res)=>{
-      res.sendFile(path.resolve(__dirname,"./","portfolio","dist", "index.html"));
-    })
-  }
-  
+const path = require("path");
 
-app.listen(process.env.PORT || 3000, ()=>{
-console.log(`server is running on port ${process.env.PORT}`)
-})
+// app.use(
+//   cors({
+//     credential: true,
+//   })
+// );
+
+app.use(express.static(path.join(__dirname, "./portfolio/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "./", "portfolio", "dist", "index.html")
+  );
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
